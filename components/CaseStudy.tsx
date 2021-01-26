9f70c67701dbe3624e6d53353864cc96a05412c5
Narendra Syahrasyad <nanda.s@hey.com>
2021-01-25 16:00:49 -0800

    feat: add footer

--- components/CaseStudy.tsx
+++ components/CaseStudy.tsx
@@ -3,6 +3,7 @@ import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa'
 import Link from 'next/link'
 import Head from 'next/head'
 import { FiHome } from 'react-icons/fi'
+import { motion } from 'framer-motion'
 
 import Circle from './Circle'
 import Dots from './Dots'
@@ -27,7 +28,7 @@ export default function CaseStudy({ meta, children }) {
         </ul>
         <Dots className="ml-auto" />
       </nav>
-      <main className={clsx(styles.main, 'mt-16')}>
+      <main className={clsx(styles.main, 'my-16 p-8')}>
         <header className="relative mb-16">
           <Circle className="w-40 h-40 bg-blue-800 -left-16 -top-6 lg:w-64 lg:h-64 lg:-left-32 lg:-top-16" />
           <div
@@ -73,8 +74,57 @@ export default function CaseStudy({ meta, children }) {
           </ul>
         </header>
         {children}
+        <footer className="relative flex justify-center p-4 pb-10 mt-16 bg-gray-800 rounded-2xl">
+          <h1 className="text-sm text-center lg:text-base">
+            Thanks for reading! For more, check out the code or play around with
+            the demo.
+          </h1>
+          <ul className="absolute flex justify-center space-x-2 text-xl -bottom-6">
+            <li>
+              <ExternalLink href={meta.github}>
+                <FaGithub />
+              </ExternalLink>
+            </li>
+            {meta.link && (
+              <li>
+                <ExternalLink href={meta.link}>
+                  <FaExternalLinkAlt />
+                </ExternalLink>
+              </li>
+            )}
+          </ul>
+        </footer>
       </main>
+      <footer>
+        <ul className="flex justify-between p-8 font-mono text-sm text-gray-500">
+          <li className="flex-1">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <Dots className="transform rotate-45" />
+          <li className="flex-1 text-right">
+            <a href="/resume.pdf">Resume</a>
+          </li>
+        </ul>
+      </footer>
     </>
+  )
+}
+
+function ExternalLink({ href, children }) {
+  return (
+    <motion.a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      className="flex items-center justify-center w-12 h-12 bg-gray-700 rounded-full hover:bg-blue-700"
+      whileHover={{
+        scale: 1.1,
+      }}
+    >
+      {children}
+    </motion.a>
   )
 }
 
